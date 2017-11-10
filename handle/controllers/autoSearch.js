9b1b0d9dc6468857f9e6fd4ed4a97adecb151770
kum <kum_cp@yahoo.com.vn>
2017-11-10 09:43:36 +0700

    Upd how to get url

--- handle/controllers/autoSearch.js
+++ handle/controllers/autoSearch.js
@@ -1,38 +1,53 @@
 const express = require('express')
 const autoSearch = express.Router()
 const { Chromeless } = require('chromeless')
+const { join } = require('path')
 
 autoSearch.post('/autosearch', (req, res) => {
 
-    let groups = req.body.data
+    let groups = req.body.data;
+    let saveDir = './data';
 
     for (let index in groups) {
         console.log(groups[index].groupName + ": " + groups[index].keywords)
     }
 
-    
-
+    searchKeyword("cat")
     res.send("Test")
 })
 
-function searchKeyword(keyword){
-    search(keyword).catch(console.error.bind(console))
-} 
+function searchKeywords(keyword) {
+    search(keyword)
+    .catch(console.error.bind(console))
+    .then((imURLs) => {
+        console.log(imURLs);
+    })
+}
 
 
 async function search(keyword) {
     const chromeless = new Chromeless()
 
-    const screenshot = await chromeless
-        .goto('https://www.google.com')
+    var imageUrlList= [];
+
+    imageUrlList = await chromeless
+        .goto('https://www.bing.com/images/discover?form=Z9LH1')
         .type(keyword, 'input[name="q"]')
-        .press(13)
-        .wait('#resultStats')
-        .screenshot()
+        .click("[name=go]")
+        .wait('[class=dg_b]')
+        .scrollToElement("footer").wait(800)
+        .evaluate((imList) => {
+            Array.prototype.forEach.call(document.getElementsByClassName("imgpt"), function(element) {
+                let imagePath = JSON.parse(element.children[0].getAttribute('m')).murl;
+                imList.push(imagePath);
+            });
+            return imList
+        }, imageUrlList);
+        
 
-    console.log(screenshot) // prints local file path or S3 url
+    await chromeless.end();
 
-    await chromeless.end()
+    return imageUrlList;
 }
 
 
